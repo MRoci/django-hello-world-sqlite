f550a7f1c545aa80865cb4f4c0047e47ecbcfb8a
Nikolaos Pothitos <pothitos@di.uoa.gr>
2017-10-13 14:33:10 +0300

    Set database

--- hello/settings.py
+++ hello/settings.py
@@ -105,3 +105,12 @@ USE_TZ = True
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'static'))
 
 STATIC_URL = '/static/'
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
